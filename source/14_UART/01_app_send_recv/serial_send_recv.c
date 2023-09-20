31d5cd5c2b2b45e84b155e4d1fff61337461ad96
zhongweizishuo <zhongweizishuo@outlook.com>
2023-09-20 11:35:26 +0800

    uart

--- source/14_UART/01_app_send_recv/serial_send_recv.c
+++ source/14_UART/01_app_send_recv/serial_send_recv.c
@@ -1,3 +1,5 @@
+// tty 设置串口通信的行规层、
+// 数据写读的回环实验
 #include <stdio.h>
 #include <string.h>
 #include <sys/types.h>
@@ -11,21 +13,21 @@
 /* set_opt(fd,115200,8,'N',1) */
 int set_opt(int fd,int nSpeed, int nBits, char nEvent, int nStop)
 {
-	struct termios newtio,oldtio;
+	struct termios newtio,oldtio; // terminal io set
 	
-	if ( tcgetattr( fd,&oldtio) != 0) { 
+	if ( tcgetattr( fd,&oldtio) != 0) { // 旧数据的保存
 		perror("SetupSerial 1");
 		return -1;
 	}
 	
-	bzero( &newtio, sizeof( newtio ) );
+	bzero( &newtio, sizeof( newtio ) ); // 数据清零，新数据设置
 	newtio.c_cflag |= CLOCAL | CREAD; 
 	newtio.c_cflag &= ~CSIZE; 
 
 	newtio.c_lflag  &= ~(ICANON | ECHO | ECHOE | ISIG);  /*Input*/
 	newtio.c_oflag  &= ~OPOST;   /*Output*/
 
-	switch( nBits )
+	switch( nBits )	//数据位的个数
 	{
 	case 7:
 		newtio.c_cflag |= CS7;
@@ -35,24 +37,24 @@ int set_opt(int fd,int nSpeed, int nBits, char nEvent, int nStop)
 	break;
 	}
 
-	switch( nEvent )
+	switch( nEvent )	//配置串口通信时对奇偶校验位进行设置的部分
 	{
-	case 'O':
+	case 'O':	//设置奇校验
 		newtio.c_cflag |= PARENB;
 		newtio.c_cflag |= PARODD;
 		newtio.c_iflag |= (INPCK | ISTRIP);
 	break;
-	case 'E': 
+	case 'E': 	//设置偶校验
 		newtio.c_iflag |= (INPCK | ISTRIP);
 		newtio.c_cflag |= PARENB;
 		newtio.c_cflag &= ~PARODD;
 	break;
-	case 'N': 
+	case 'N': 	// 禁用
 		newtio.c_cflag &= ~PARENB;
 	break;
 	}
 
-	switch( nSpeed )
+	switch( nSpeed )  //设置波特率
 	{
 	case 2400:
 		cfsetispeed(&newtio, B2400);
@@ -76,11 +78,13 @@ int set_opt(int fd,int nSpeed, int nBits, char nEvent, int nStop)
 	break;
 	}
 	
+	// 设置校验位
 	if( nStop == 1 )
 		newtio.c_cflag &= ~CSTOPB;
 	else if ( nStop == 2 )
 		newtio.c_cflag |= CSTOPB;
 	
+	// 数据读取的长度与等待时间
 	newtio.c_cc[VMIN]  = 1;  /* 读数据时的最小字节数: 没读到这些数据我就不返回! */
 	newtio.c_cc[VTIME] = 0; /* 等待第1个数据的时间: 
 	                         * 比如VMIN设为10表示至少读到10个数据才返回,
@@ -90,7 +94,7 @@ int set_opt(int fd,int nSpeed, int nBits, char nEvent, int nStop)
 	                         *    如果10秒内至少读到了1个字节，那就继续等待，完全读到VMIN个数据再返回
 	                         */
 
-	tcflush(fd,TCIFLUSH);
+	tcflush(fd,TCIFLUSH);//清除数据
 	
 	if((tcsetattr(fd,TCSANOW,&newtio))!=0)
 	{
@@ -110,7 +114,7 @@ int open_port(char *com)
 		return(-1);
     }
 	
-	  if(fcntl(fd, F_SETFL, 0)<0) /* 设置串口为阻塞状态*/
+	  if(fcntl(fd, F_SETFL, 0)<0) /* 设置串口为阻塞状态： 数据读写不成功就会休眠*/
 	  {
 			printf("fcntl failed!\n");
 			return -1;
@@ -153,7 +157,7 @@ int main(int argc, char **argv)
 		return -1;
 	}
 
-	iRet = set_opt(fd, 115200, 8, 'N', 1);
+	iRet = set_opt(fd, 115200, 8, 'N', 1);//设置行规层参数
 	if (iRet)
 	{
 		printf("set port err!\n");
@@ -161,6 +165,8 @@ int main(int argc, char **argv)
 	}
 
 	printf("Enter a char: ");
+	
+	// 数据发送与读取，短接数据口，实现回环，发出即收到
 	while (1)
 	{
 		scanf("%c", &c);
--- source/14_UART/02_gps/gps_read.c
+++ source/14_UART/02_gps/gps_read.c
@@ -1,3 +1,4 @@
+// 串口GPS模块实验
 #include <stdio.h>
 #include <string.h>
 #include <sys/types.h>
@@ -122,23 +123,24 @@ int open_port(char *com)
 
 int read_gps_raw_data(int fd, char *buf)
 {
+	// 读取原始数据，存入一个buf
 	int i = 0;
 	int iRet;
 	char c;
 	int start = 0;
 	
-	while (1)
+	while (1)// 数据持续读取
 	{
-		iRet = read(fd, &c, 1);
+		iRet = read(fd, &c, 1);//读取一个数据保存进入字符c
 		if (iRet == 1)
 		{
-			if (c == '$')
+			if (c == '$')//开始标志
 				start = 1;
 			if (start)
 			{
-				buf[i++] = c;
+				buf[i++] = c; //c持续写入buf
 			}
-			if (c == '\n' || c == '\r')
+			if (c == '\n' || c == '\r') //见到回车 换行，数据读取结束
 				return 0;
 		}
 		else
@@ -155,15 +157,23 @@ int parse_gps_raw_data(char *buf, char *time, char *lat, char *ns, char *lng, ch
 	
 	if (buf[0] != '$')
 		return -1;
-	else if (strncmp(buf+3, "GGA", 3) != 0)
+	else if (strncmp(buf+3, "GGA", 3) != 0) // 第三个字符开始必须是GGA
 		return -1;
 	else if (strstr(buf, ",,,,,"))
 	{
+		// strstr()找出",,,,,"第一次在buf里面出现的位置
 		printf("Place the GPS to open area\n");
 		return -1;
 	}
 	else {
 		//printf("raw data: %s\n", buf);
+		// 从buf中读取格式化输入，数据存入形参buf,这些buf在main()中提供
+		
+		#if 0
+		sscanf(buf, "%s, %s, %s, %s, %s, %s", tmp, time, lat, ns, lng, ew);
+		// 数据读取错误，sscanf()需要剔除原始数据中的,也就是写成 %[^,]
+		#endif
+
 		sscanf(buf, "%[^,],%[^,],%[^,],%[^,],%[^,],%[^,]", tmp, time, lat, ns, lng, ew);
 		return 0;
 	}
@@ -179,6 +189,8 @@ int main(int argc, char **argv)
 	int iRet;
 	char c;
 	char buf[1000];
+	
+	// raw_data解析的buf
 	char time[100];
 	char Lat[100]; 
 	char ns[100]; 
@@ -187,6 +199,8 @@ int main(int argc, char **argv)
 
 	float fLat, fLng;
 
+	// main() 的实现步骤:
+	
 	/* 1. open */
 
 	/* 2. setup 
@@ -218,13 +232,16 @@ int main(int argc, char **argv)
 		return -1;
 	}
 
+	// 读写数据与数据处理
 	while (1)
 	{
-		/* eg. $GPGGA,082559.00,4005.22599,N,11632.58234,E,1,04,3.08,14.6,M,-5.6,M,,*76"<CR><LF>*/
+		/* eg. $GPGGA,082559.00,4005.22599,N,11632.58234,E,1,04,3.08,14.6,M,-5.6,M,,*76"<CR><LF>
+		<CR><LF>结尾，表示回车换行
+		*/
 		/* read line */
 		iRet = read_gps_raw_data(fd, buf);
 		
-		/* parse line */
+		/* parse line  数据解析*/
 		if (iRet == 0)
 		{
 			iRet = parse_gps_raw_data(buf, time, Lat, ns, Lng, ew);
@@ -239,16 +256,18 @@ int main(int argc, char **argv)
 			printf("Lat  : %s\n", Lat);
 			printf("Lng  : %s\n", Lng);
 
-			/* 纬度格式: ddmm.mmmm */
+			/* 纬度格式: ddmm.mmmm ；将分（mm.mmmm）转换为dd  */
 			sscanf(Lat+2, "%f", &fLat);
 			fLat = fLat / 60;
 			fLat += (Lat[0] - '0')*10 + (Lat[1] - '0');
 
-			/* 经度格式: dddmm.mmmm */
+			/* 经度格式: dddmm.mmmm ；将分（mm.mmmm）转换为dd  */
 			sscanf(Lng+3, "%f", &fLng);
 			fLng = fLng / 60;
 			fLng += (Lng[0] - '0')*100 + (Lng[1] - '0')*10 + (Lng[2] - '0');
-			printf("Lng,Lat: %.06f,%.06f\n", fLng, fLat);
+			// (Lng[0] - '0')为了得到数值
+			
+			printf("Lng, Lat: %.06f,%.06f\n", fLng, fLat);
 		}
 	}
 
